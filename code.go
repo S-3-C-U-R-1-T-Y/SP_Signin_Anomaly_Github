73ca8621ba47c4acc064e2cd7f649ce99d28aac2
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-05-16 07:11:15 +0200

    Create code.go

---
+++ code.go
@@ -0,0 +1,5 @@
+SP_Signin_Anomaly_Github
+"AadClientId": "90a5126b-f770-4727-9c73-7327077fdaed",
+"AadSecret": "yx~8Q~xuJyQfcXwx4KcQmALYCgAcs7uDU5w3ncxA",
+"AadTenantDomain": "nedscaperlab.onmicrosoft.com",
+"AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
